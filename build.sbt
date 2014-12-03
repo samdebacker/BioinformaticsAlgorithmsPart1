edbb087ce2311d8afa07f80d94fe5ae57db678ce
Sam De Backer <sam@ireact.be>
2014-12-03 21:45:11 +0100

    motifEnumeration

--- build.sbt
+++ build.sbt
@@ -4,9 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.4"
 
-// Change this to another test framework if you prefer
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+
+// display test duration
+testOptions in Test += Tests.Argument("-oD")
 
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.3.3"
-
--- src/main/scala/Week3.scala
+++ src/main/scala/Week3.scala
@@ -2,4 +2,14 @@ import scala.math._
 
 object Week3 {
   def expectedNumberOfOccurences(k: Int, L: Int, n: Int): Double = (L - k + 1) * pow(.25, k) * n
+
+  type DNA = Set[String]
+  def motifEnumeration(dna: DNA, k: Int, d: Int): Set[String] = {
+    for {
+      singleDna <- dna
+      i <- 0 until (singleDna.length - k)
+      kMer = singleDna.substring(i, i + k)
+      neighbour <- Week1.neighbours(kMer, d) if dna.forall(Week1.countApproxPattern(_, neighbour, d) > 0)
+    } yield neighbour
+  }
 }
